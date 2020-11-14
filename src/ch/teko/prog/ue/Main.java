7eadae0ea5e0d349261ed897dff6aef2ff183119
Heiven Kifle <heiven.kifle@edu.teko.ch>
2020-11-14 09:47:22 +0100

    Initial commit

---
+++ src/ch/teko/prog/ue/Main.java
@@ -0,0 +1,14 @@
+package ch.teko.prog.ue;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        String input = "";
+        while (!input.toLowerCase().equals("stop")){
+            input = new Scanner(System.in).next();
+            System.out.println(input);
+        }
+    }
+}
