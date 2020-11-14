c03ab5d235f705a44ae21db61343177054085d61
HeivenK <74095358+HeivenK@users.noreply.github.com>
2020-11-14 11:26:13 +0100

    Update Main.java

--- src/ch/teko/prog/ue/Main.java
+++ src/ch/teko/prog/ue/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args){
         String input = "";
-        while (!input.toLowerCase().equals("stop")){
+        while (!input.toLowerCase().equals("stop ")){
             input = new Scanner(System.in).next();
             System.out.println(input);
             System.out.println(" ");
